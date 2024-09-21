a715a57ad2c7fa6a676ee7cbe3e4e08ec9346143
Dirakon <609700zxc@mail.ru>
2024-09-21 16:34:59 +0300

    More neotest keybinds

--- lua/myLuaConf/plugins/neotest.lua
+++ lua/myLuaConf/plugins/neotest.lua
@@ -10,3 +10,6 @@ vim.keymap.set('n', '<leader>Tr', (function() require("neotest").run.run() end),
 vim.keymap.set('n', '<leader>TR', (function() require("neotest").run.run(vim.fn.expand("%")) end), { desc = 'Neo[T]est - [R]un all in file' })
 vim.keymap.set('n', '<leader>Ts', (function() require("neotest").run.stop() end), { desc = 'Neo[T]est - [S]top' })
 vim.keymap.set('n', '<leader>Td', (function() require("neotest").run.run({strategy = "dap"}) end), { desc = 'Neo[T]est - [D]ebug closest' })
+vim.keymap.set('n', '<leader>Ts', (function() vim.cmd("Neotest summary") end), { desc = 'Neo[T]est - [S]ummary panel to right' })
+vim.keymap.set('n', '<leader>TO', (function() vim.cmd("Neotest output-panel") end), { desc = 'Neo[T]est - [O]utput panel' })
+vim.keymap.set('n', '<leader>To', (function() vim.cmd("Neotest output") end), { desc = 'Neo[T]est - [O]utput popup' })
