f3c29d9bb0c7a66211b1ae656e878fded4681552
Dirakon <609700zxc@mail.ru>
2024-10-08 21:49:25 +0300

    Setup gdscript lsp

--- lua/myLuaConf/LSPs/init.lua
+++ lua/myLuaConf/LSPs/init.lua
@@ -1,53 +1,4 @@
 local servers = {}
--- if nixCats('neonixdev') then
---   -- NOTE: Lazydev will make your lua lsp stronger for neovim config
---   -- NOTE: we are also using this as an opportunity to show you how to lazy load plugins!
---   -- This plugin was added to the optionalPlugins section of the main flake.nix of this repo.
---   -- Thus, it is not loaded and must be packadded.
---   vim.api.nvim_create_autocmd('FileType', {
---     group = vim.api.nvim_create_augroup('nixCats-lazydev', { clear = true }),
---     pattern = { 'lua' },
---     callback = function(event)
---       -- NOTE: Use `:NixCats pawsible` to see the names of all plugins downloaded via nix for packadd
---       vim.cmd.packadd('lazydev.nvim')
---       require('lazydev').setup({
---         library = {
---         --   -- See the configuration section for more details
---         --   -- Load luvit types when the `vim.uv` word is found
---         --   -- { path = "luvit-meta/library", words = { "vim%.uv" } },
---           -- adds type hints for nixCats global
---           { path = require('nixCats').nixCatsPath .. '/lua', words = { "nixCats" } },
---         },
---       })
---     end
---   })
---   -- NOTE: use nvim-neorocks/lz.n to manage the autocommands for you if the above seems tedious.
---   -- Or, use the wrapper for lazy.nvim included in the luaUtils template.
---   -- NOTE: AFTER DIRECTORIES WILL NOT BE SOURCED BY PACKADD!!!!!
---   -- this must be done by you manually if,
---   -- for example, you wanted to lazy load nvim-cmp sources
---
---   servers.lua_ls = {
---     Lua = {
---       formatters = {
---         ignoreComments = true,
---       },
---       signatureHelp = { enabled = true },
---       diagnostics = {
---         globals = { 'nixCats' },
---         disable = { 'missing-fields' },
---       },
---     },
---     telemetry = { enabled = false },
---     filetypes = { 'lua' },
---   }
---   if require('nixCatsUtils').isNixCats then servers.nixd = {}
---   else servers.rnix = {}
---   end
---   servers.nil_ls = {}
---
--- end
---
 servers.bashls = {
   cmd = {
     "bash-language-server",
@@ -69,10 +20,7 @@ servers.lua_ls = {
   telemetry = { enabled = false },
   filetypes = { 'lua' },
 }
--- if require('nixCatsUtils').isNixCats then
 servers.nixd = {} -- nix (i think)
--- else servers.rnix = {}
--- end
 servers.nil_ls = {}  -- nix (i think)
 servers.jsonls = {}  -- json
 servers.ts_ls = { -- ts/js
@@ -89,19 +37,14 @@ servers.ts_ls = { -- ts/js
 
 }
 
--- This is this flake's version of what kickstarter has set up for mason handlers.
--- This is a convenience function that calls lspconfig on the lsps we downloaded via nix
--- This will not download your lsp. Nix does that.
-
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---  All of them are listed in https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
---  You may do the same thing with cmd
-
 servers.clangd = {} -- c(++)
+servers.gdscript = {} -- godot, gdscript
+local pipepath = vim.fn.stdpath("cache") .. "/godot-server.pipe"
+if not vim.loop.fs_stat(pipepath) then
+  vim.fn.serverstart(pipepath)
+end
+
+
 -- servers.gopls = {},
 -- servers.pyright = {},
 -- servers.rust_analyzer = {},
